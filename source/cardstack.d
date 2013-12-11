d4d1c4489c48cc965b9e12e8f46338d08e7bb942
Uplink.Coder <uplink.coder@gmail.com>
2013-12-11 10:32:53 +0100

    added OwendCardStack

--- source/cardstack.d
+++ source/cardstack.d
@@ -1,5 +1,6 @@
 import _containers._stack;
 public import SkiboCard:SkiboCard;
+import Player:Player;
 import std.exception:enforce;
 
 abstract class CardStack : Stack!SkiboCard {
@@ -8,34 +9,47 @@ abstract class CardStack : Stack!SkiboCard {
 		final @property SkiboCard view() {
 			return (top);
 		}  
-	}
-	
-	interface Playable : Viewable {
-		/** drops the to card of this onto the Dropable Stack */
-		final void playCard(CardStack.Dropable stack) {
-			stack.drop(pop);
-		}
-	}
+	}	
 	
 	interface Dropable : Viewable {
 		bool dropCondition(SkiboCard card);
-		final void drop(SkiboCard card) {
+		final void push(SkiboCard card) {
 			enforce(dropCondition(card),"dropCondition was not statisfaid"); 
-				push(card);
+				super.push(card);
 		}
 	}
+	/*
 	final private @property SkiboCard top(){assert(false,"top is not to be called");}
 	final private @property SkiboCard pop(){assert(false,"pop is not to be called");}
 	final private @property void push(SkiboCard card){assert(false,"push is not to be called");}
+	*/ 
 }
-class PlayerStack:CardStack,CardStack.Playable {
+
+abstract class OwendCardStack:CardStack {
+	Player owner;
+	//auto peqo = delegate () {return p==owner||owner==null;};
+	final @property SkiboCard push(Player p,SkiboCard card){if(peqo){super.push(card);}}
+	final @property SkiboCard push(Player p,SkiboCard card){if(peqo){super.push(card);}}
+	final @property SkiboCard push(Player p,SkiboCard card){if(peqo){super.push(card);}}
+	
+	interface Playable : Viewable {
+		/** drops the to card of this onto the Dropable Stack */
+		final void pop(CardStack.Dropable stack) {
+			stack.drop(super.pop);
+		}
+	}
+	
+	class PlayerStack:OwendCardStack,OwendCardStack.Playable {
 	this(SkiboCard[] init) {
 		foreach (card;init) super.push(card);
 		}
 	}
 
-class SupportStack : CardStack, CardStack.Dropable, CardStack.Playable {
+	class SupportStack : CardStack, CardStack.Dropable, CardStack.Playable {
 	bool dropCondition(SkiboCard c) {return true;}	 
+	}
+		
+		
 }
 
 class DropStack : CardStack,CardStack.Dropable {
